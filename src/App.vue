2ef934067b6658ba764f9d644f530edc4cabfbd0
Anson Ng <914anson5@gmail.com>
2023-02-13 19:50:05 +0800

    feat: change default sans font to General Sans
    This also adds default css styles to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-
+  <link
+    href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&display=swap"
+    rel="stylesheet"
+  />
+  <div id="app" class="flex h-screen min-h-[392px] flex-col">
+    <router-view />
+  </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['Archivo', 'Arial', 'sans-serif'],
-      mono: ['JetBrains\\ Mono'],
+      sans: ["General\\ Sans", "Arial", "sans-serif"],
+      mono: ["JetBrains\\ Mono"],
     },
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
