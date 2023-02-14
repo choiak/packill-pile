8af5c4df3661d7a060cb05ba358df50da050a11d
Anson Ng <914anson5@gmail.com>
2023-02-14 20:17:25 +0800

    feat: add Vue component for login

--- src/views/session/login.vue
+++ src/views/session/login.vue
@@ -22,7 +22,7 @@
         />
         <button
           type="submit"
-          class="btn-white bg-blue-600 text-sm font-medium text-white hover:bg-blue-700 active:shadow-blue-900"
+          class="btn bg-blue-600 text-sm font-medium text-white hover:bg-blue-700 active:shadow-blue-900"
         >
           Sign in
         </button>
@@ -37,7 +37,7 @@
         <div>
           <a href="/pile/register">
             <button
-              class="btn-white bg-white text-sm font-medium text-neutral-700 hover:bg-slate-400/20 active:shadow-slate-300"
+              class="btn bg-white text-sm font-medium text-neutral-700 hover:bg-slate-400/20 active:shadow-slate-300"
             >
               Create new account
             </button>
