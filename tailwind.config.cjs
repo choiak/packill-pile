de3af61b82132ddb19cfd61f3cee923e66500212
Anson Ng <914anson5@gmail.com>
2023-03-14 10:43:41 +0800

    feat: change 'mono' font family from 'JetBrains Mono' to 'Azeret Mono', add .prose-article style class to style.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-    theme: {
-        fontFamily: {
-            sans: ["General\\ Sans", "Arial", "sans-serif"],
-            text: ["Supreme"],
-            mono: ["JetBrains\\ Mono"],
-        },
-        extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/forms"),
-        require('@tailwindcss/typography'),
-    ],
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		fontFamily: {
+			sans: ['General\\ Sans', 'Arial', 'sans-serif'],
+			text: ['Supreme'],
+			mono: ['Azeret\\ Mono'],
+		},
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+	],
 };
