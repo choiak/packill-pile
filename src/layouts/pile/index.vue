a24679dd874b63a0cc85824e3f5b2b29a06cac65
Anson Ng <914anson5@gmail.com>
2023-03-14 20:57:38 +0800

    feat: modify index.vue to support responsive design

--- src/layouts/pile/index.vue
+++ src/layouts/pile/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="h-full bg-stone-100">
-		<div class="flex h-full min-h-fit overflow-auto p-6 sm:space-x-6">
+		<div class="flex h-full min-h-fit overflow-auto sm:p-6 sm:space-x-6">
 			<Menubar class='hidden sm:flex'/>
 			<div class="flex-1">
 				<slot />
