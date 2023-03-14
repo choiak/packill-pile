dd21078755b2da7827e2a69cda4f53778e313110
Anson Ng <914anson5@gmail.com>
2023-03-14 10:45:48 +0800

    feat: add Vue directive 'v-highlight' to enable syntax highlighting for code blocks

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router/index";
-import VueClickAway from "vue3-click-away";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
+import App from './App.vue';
+import router from './router/index';
+import VueClickAway from 'vue3-click-away';
+import hl from 'highlight.js';
+import 'highlight.js/styles/stackoverflow-light.css';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -12,4 +14,11 @@ app.use(router);
 app.use(pinia);
 app.use(VueClickAway);
 
-app.mount("#app");
+app.directive('highlight', function (el) {
+	const blocks = el.querySelectorAll('code');
+	blocks.forEach((block) => {
+		hl.highlightBlock(block);
+	});
+});
+
+app.mount('#app');
