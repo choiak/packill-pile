c8b8f61888731267e4bb7e2264f9379f94595f12
Anson Ng <914anson5@gmail.com>
2023-03-14 11:18:52 +0800

    feat: add Index.vue layout

---
+++ src/layouts/pile/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="h-full bg-stone-100">
+		<div class="flex h-full min-h-fit overflow-auto p-6 sm:space-x-6">
+			<Menubar class='hidden sm:flex'/>
+			<div class="flex-1">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import Menubar from '@/layouts/menuBar/menubar.vue';
+import Topic from '@/layouts/pile/topic.vue';
+</script>
