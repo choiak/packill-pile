7d5729cb914a098a380cd11fcc089aab18558653
Anson Ng <914anson5@gmail.com>
2023-03-14 10:44:59 +0800

    feat: limit component max-width to 2560px in App.vue, fetch new font 'Supreme' and 'Azeret Mono'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup></script>
 
 <template>
-  <link
-    href="https://api.fontshare.com/v2/css?f[]=general-sans@1,2&display=swap"
-    rel="stylesheet"
-  />
-  <div id="app" class="flex h-screen min-h-[392px] flex-col">
-    <router-view />
-  </div>
+	<link
+		href="https://api.fontshare.com/v2/css?f[]=general-sans@1,2&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		href="https://api.fontshare.com/v2/css?f[]=supreme@2,1&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		href="https://api.fontshare.com/v2/css?f[]=azeret-mono@2,1&display=swap"
+		rel="stylesheet"
+	/>
+	<Suspense>
+		<div id="app" class="flex justify-center bg-stone-100 w-screen h-screen min-h-[392px]">
+			<router-view class='max-w-[2560px]'/>
+		</div>
+	</Suspense>
 </template>
