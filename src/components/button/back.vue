77f118326132ecb3aa67f96c083fa57dc8a12946
Anson Ng <914anson5@gmail.com>
2023-03-14 11:29:20 +0800

    feat: add back.vue component for universal back button (subject to change)

---
+++ src/components/button/back.vue
@@ -0,0 +1,9 @@
+<template>
+	<button class="btn-menu">
+		<ArrowLeftCircleIcon class="h-5 w-5" />
+	</button>
+</template>
+
+<script setup>
+import { ArrowLeftCircleIcon } from '@heroicons/vue/24/outline';
+</script>
