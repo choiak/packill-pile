0857591877229f5d6c9b7973c1c3b1b22c46b6be
Anson Ng <914anson5@gmail.com>
2023-04-22 20:45:06 +0800

    feat: add new REST API calls and update old ones to use new API call function from fetch.js

--- src/api/workspace.js
+++ src/api/workspace.js
@@ -1,86 +1,194 @@
 import qs from 'qs';
 import { requestWIthValidation } from '@/utils/request.js';
+import { useFetch } from '@/utils/fetch.js';
 
-export async function getTopic(id) {
+export function getProblem(id) {
 	const query = qs.stringify(
 		{
 			populate: {
-				knowledges: {
+				content: {
 					populate: {
-						author: {
-							populate: ['avatar'],
+						question: {
+							fields: ['id'],
 						},
-						areas: true,
-					}
-				}
-			}
+					},
+				},
+				difficulty: true,
+			},
 		},
 		{
 			encodeValuesOnly: true, // prettify URL
-		}
-	)
+		},
+	);
 
-	return await requestWIthValidation
-		.get(
-			`/api/topics/${id}?${query}`,
-		)
-		.then((res) => {
-			return res.data.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	return useFetch(
+		`http://localhost:1337/api/problems/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
 }
 
-export async function getPackage(id) {
+export function getProblems(config = {}) {
+	const query = qs.stringify(
+		{
+			...config,
+		},
+		{
+			encodeValuesOnly: true, // prettify URL
+		},
+	);
+
+	return useFetch(
+		`http://localhost:1337/api/problems/?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function getDifficulty(id) {
+	const query = qs.stringify(
+		{},
+		{
+			encodeValuesOnly: true, // prettify URL
+		},
+	);
+
+	return useFetch(
+		`http://localhost:1337/api/difficulties/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function getKnowledge(id) {
+	const query = qs.stringify(
+		{
+			populate: {
+				author: {
+					populate: ['avatar'],
+				},
+				areas: true,
+			},
+		},
+		{
+			encodeValuesOnly: true, // prettify URL
+		},
+	);
+
+	return useFetch(
+		`http://localhost:1337/api/knowledges/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function getTopic(id) {
+	const query = qs.stringify(
+		{
+			populate: {
+				knowledges: true,
+				problems: {
+					fields: ['id'],
+				},
+			},
+		},
+		{
+			encodeValuesOnly: true, // prettify URL
+		},
+	);
+
+	return useFetch(
+		`http://localhost:1337/api/topics/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function getPackage(id) {
 	const query = qs.stringify(
 		{
 			populate: ['areas'],
 		},
 		{
 			encodeValuesOnly: true, // prettify URL
-		}
-	)
+		},
+	);
 
-	return await requestWIthValidation
-		.get(
-			`/api/packages/${id}?${query}`,
-		)
-		.then((res) => {
-			return res.data.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	return useFetch(
+		`http://localhost:1337/api/packages/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
 }
 
-export async function listPackages() {
+export function getPartition(id) {
 	const query = qs.stringify(
 		{
-			populate: ['areas'],
+			populate: ['topics'],
 		},
 		{
 			encodeValuesOnly: true, // prettify URL
-		}
-	)
+		},
+	);
 
-	return await requestWIthValidation
-		.get(
-			`/api/packages?${query}`,
-		)
-		.then((res) => {
-			return res.data.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	return useFetch(
+		`http://localhost:1337/api/partitions/${id}?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
 }
 
-export async function listAreas() {
+export function listPackages() {
+	const query = qs.stringify(
+		{
+			populate: ['areas'],
+			fields: ['name', 'description', 'areas', 'certificated'],
+		},
+		{
+			encodeValuesOnly: true, // prettify URL
+		},
+	);
+
+	return useFetch(
+		`http://localhost:1337/api/packages/?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function listAreas() {
+	return useFetch(
+		`http://localhost:1337/api/areas`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export async function selectPackage(id) {
 	return await requestWIthValidation
-		.get(
-			`/api/areas/`,
-		)
+		.put(`/api/users/1`, {
+			currentPackage: {
+				set: [id],
+			},
+		})
 		.then((res) => {
 			return res.data.data;
 		})
