84a559019128f01818207cec68e8a4e3ccf416a7
Anson Ng <914anson5@gmail.com>
2023-04-22 20:55:33 +0800

    feat: add info store in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import { defineStore } from 'pinia';
 import { reactive } from 'vue';
 
-export const useUserStore = defineStore('user', () => {
-	let user = reactive({
-		id: '',
-		username: '',
-		displayName: '',
-	});
+export const useUserStore = defineStore(
+	'user',
+	() => {
+		let user = reactive({
+			id: '',
+			username: '',
+			displayName: '',
+		});
 
-	return { user };
+		return { user };
+	},
+	{
+		persist: {
+			storage: sessionStorage,
+		},
+	},
+);
+
+export const useInfoStore = defineStore('info', () => {
+	const url = {
+		strapi: 'http://localhost:1337',
+	};
+
+	return { url };
 });
