4840f41743b464a416096bb5df864946df33f184
Anson Ng <914anson5@gmail.com>
2023-04-22 20:53:42 +0800

    feat: add new focus vue directive for auto-focus on show

---
+++ src/components/venust/expandable/expandable.vue
@@ -0,0 +1,37 @@
+<template>
+	<div
+		class="w-full border bg-slate-50 p-4 transition first:rounded-t-lg last:rounded-b-lg"
+		:class="{ shadow: isActive }"
+		@click="toggle"
+	>
+		<div class="flex items-center justify-between">
+			<div class="flex items-center space-x-4">
+				<slot name="icon" />
+				<slot name="header" />
+			</div>
+			<ArrowSmallRightIcon
+				class="h-5 w-5 text-neutral-700 transition"
+				:class="{ 'rotate-90': isActive }"
+			/>
+		</div>
+	</div>
+	<Transition name="fade">
+		<div
+			class="w-full border px-8 py-8 last:rounded-b-lg"
+			v-show="isActive"
+		>
+			<slot name="content" />
+		</div>
+	</Transition>
+</template>
+
+<script setup>
+import { ArrowSmallRightIcon } from '@heroicons/vue/24/outline/index.js';
+import { ref } from 'vue';
+
+const isActive = ref(false);
+
+function toggle() {
+	isActive.value = !isActive.value;
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import './style.css';
 import App from './App.vue';
 import router from './router/index';
-import VueClickAway from 'vue3-click-away';
-import hl from 'highlight.js';
 import 'highlight.js/styles/stackoverflow-light.css';
+import InstantSearch from 'vue-instantsearch/vue3/es';
 
 const app = createApp(App);
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 app.use(router);
 app.use(pinia);
-app.use(VueClickAway);
+app.use(InstantSearch);
 
-app.directive('highlight', function (el) {
-	const blocks = el.querySelectorAll('code');
-	blocks.forEach((block) => {
-		hl.highlightBlock(block);
-	});
+app.directive('focus', {
+	mounted: function (el) {
+		el.focus();
+	},
 });
 
 app.mount('#app');
