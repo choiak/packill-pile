a79681b0f0203980c041a1a7a0d8ccf7993dd1cb
Anson Ng <914anson5@gmail.com>
2023-04-22 20:38:15 +0800

    feat: move layouts/index.vue and redesign its UI

---
+++ src/layouts/utils/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="h-full w-full bg-stone-50">
+		<div class="flex h-full min-h-fit w-full">
+			<Menubar class="hidden sm:flex" />
+			<div class="flex-1 overflow-auto">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import Menubar from '@/layouts/menuBar/menubar.vue';
+import Topic from '@/layouts/topic/topic.vue';
+import Dock from '@/layouts/dock/dock.vue';
+</script>
