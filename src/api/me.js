38ee620f4f62c5a7fec03d78edf8c184773dc205
Anson Ng <914anson5@gmail.com>
2023-04-22 20:16:12 +0800

    feat: add me.js for API calls related to current user

---
+++ src/api/me.js
@@ -0,0 +1,43 @@
+import qs from 'qs';
+import { useFetch } from '@/utils/fetch.js';
+
+export function getMyProgress(config = {}) {
+	const query = qs.stringify({
+		fields: ['currentPackage', 'currentPartition', 'currentTopic'],
+		populate: {
+			currentTopic: true,
+			currentPackage: true,
+			currentPartition: {
+				populate: {
+					topics: true,
+				},
+			},
+		},
+		...config,
+	});
+
+	return useFetch(
+		`http://localhost:1337/api/users/me?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
+
+export function getMe(config = {}) {
+	const query = qs.stringify({
+		populate: {
+			avatar: true,
+		},
+		...config,
+	});
+
+	return useFetch(
+		`http://localhost:1337/api/users/me?${query}`,
+		{
+			method: 'GET',
+		},
+		true,
+	);
+}
