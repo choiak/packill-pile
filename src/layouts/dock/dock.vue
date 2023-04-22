abcbd69f57afe1dbba6a416df6a559bc0c812970
Anson Ng <914anson5@gmail.com>
2023-04-22 20:25:24 +0800

    feat: add new layout dock.vue

---
+++ src/layouts/dock/dock.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex items-center justify-between border-b bg-white px-6 py-4">
+		<div class="flex items-end">
+			<slot />
+		</div>
+		<Search />
+	</div>
+</template>
+
+<script setup>
+import VenustInput from '@/components/venust/input/venustInput.vue';
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline/index.js';
+import Search from '@/layouts/search/search.vue';
+</script>
