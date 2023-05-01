13685b78fa9a7e44b6b9ef8e41bf3f15e66504f4
Anson Ng <914anson5@gmail.com>
2023-05-01 19:14:31 +0800

    feat: new Venust UI component: Tooltip

---
+++ src/components/venust/tooltip/venustTooltip.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<div class='relative'>
+			<div @mouseover='open' @mouseleave='close' ref='elReference'>
+				<slot name='reference' />
+			</div>
+			<transition name='fade'>
+				<div
+					class='flex items-center rounded-lg space-x-1 px-2 py-1 text-sm text-white bg-neutral-950/75 absolute max-w-[300px] w-max shadow'
+					v-show='isActive' ref='elTooltip'>
+					<slot name='tooltip' />
+				</div>
+			</transition>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { nextTick, onUnmounted, ref, unref } from 'vue';
+import { createPopper } from '@popperjs/core';
+
+const props = defineProps({
+	modifiers: {
+		type: Array,
+		default: [],
+	},
+});
+
+const isActive = ref(false);
+
+function open() {
+	createPopperInstance();
+	isActive.value = true;
+}
+
+function close() {
+	isActive.value = false;
+}
+
+const elReference = ref(null);
+const elTooltip = ref(null);
+let popperInstance = null;
+
+const createPopperInstance = () => {
+	popperInstance = createPopper(unref(elReference), unref(elTooltip), {
+		modifiers: [
+			{
+				name: 'offset',
+				options: {
+					offset: [0, 4],
+				},
+			},
+			...props.modifiers,
+		],
+	});
+	nextTick(() => {
+		popperInstance.update();
+	});
+};
+
+const destroyPopperInstance = () => {
+	popperInstance?.destroy?.();
+	popperInstance = null;
+};
+
+onUnmounted(() => {
+	destroyPopperInstance();
+});
+</script>
