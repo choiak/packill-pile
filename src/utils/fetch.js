345f9ba4fdf88d3eba99ad111c14a956a4971e12
Anson Ng <914anson5@gmail.com>
2023-05-02 12:06:30 +0800

    chore: remove old comments in fetch.js

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -17,26 +17,3 @@ export const useFetchValidated = createFetch({
 		},
 	},
 });
-// const cacheMap = reactive(new Map())
-// const useCachedFetch = (key, url, config) => {
-// 	const info = useFetch(url, {skip: true, ...config});
-//
-// 	const update = () => cacheMap.set(key, info.response.value);
-// 	const clear = () => cacheMap.set(key, undefined);
-//
-// 	const fetch = async () => {
-// 		try {
-// 			await info.fetch()
-// 			update()
-// 		} catch {
-// 			clear()
-// 		}
-// 	}
-//
-// 	const response = computed(() => cacheMap.get(key))
-// 	const data = computed(() => response.value?.data)
-//
-// 	if (response.value === null) fetch()
-//
-// 	return { ...info, fetch, data, response, clear }
-// }
