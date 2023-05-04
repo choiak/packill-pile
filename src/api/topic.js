4b59aa7115be90b0da51a3c301196e51d303b392
Anson Ng <914anson5@gmail.com>
2023-05-04 18:08:37 +0800

    feat: allow to pass in useFetch config in topic.js

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,15 +1,18 @@
 import qs from 'qs';
 import { useFetchValidated } from '@/utils/fetch.js';
+import { computed, unref } from 'vue';
 
-export function getTopic(id, config = {}) {
-	const query = qs.stringify(
-		{
-			...config,
-		},
+export function getTopic(id, query = {}, config = {}) {
+	const queryString = qs.stringify(
+		query,
 		{
 			encodeValuesOnly: true, // prettify URL
 		},
 	);
 
-	return useFetchValidated(`api/topics/${id}?${query}`).get().json();
+	const url = computed(() => {
+		return `api/topics/${unref(id)}?${queryString}`;
+	})
+
+	return useFetchValidated(url, config).get().json();
 }
