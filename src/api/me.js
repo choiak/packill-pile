f2ff20967f3fabf5ff69c75572cee296f17a0228
Anson Ng <914anson5@gmail.com>
2023-05-08 22:53:55 +0800

    feat: allow to pass in useFetch config in me.js

--- src/api/me.js
+++ src/api/me.js
@@ -2,93 +2,59 @@ import qs from 'qs';
 import { useFetchValidated } from '@/utils/fetch.js';
 import { useUserStore } from '@/store/index.js';
 
-export function getMyProgress(config = {}) {
-	const query = qs.stringify({
-		fields: ['currentPackage', 'currentPartition', 'currentTopic'],
-		populate: {
-			currentTopic: true,
-			currentPackage: true,
-			currentPartition: {
-				populate: {
-					topics: true,
-				},
-			},
-		},
-		...config,
+export function getMe(query = {}, config = {}) {
+	const queryString = qs.stringify(query, {
+		encodeValuesOnly: true,
 	});
 
-	return useFetchValidated(`api/users/me?${query}`).get().json();
+	return useFetchValidated(`api/users/me?${queryString}`, config).get().json();
 }
 
-export function getMe(config = {}) {
-	const query = qs.stringify({
-		...config,
-	});
-
-	return useFetchValidated(`api/users/me?${query}`).get().json();
-}
-
-export function updateMe(content, config = {}) {
+export function updateMe(content, query = {}, config = {}) {
 	const userStore = useUserStore();
-	const query = qs.stringify({
-			...config,
-		},
-		{
-			encodeValuesOnly: true, // prettify URL
-		});
+	const queryString = qs.stringify(query, {
+		encodeValuesOnly: true, // prettify URL
+	});
 
-	return useFetchValidated(`api/users/${userStore.user.id}?${query}`).put({
+	return useFetchValidated(`api/users/${userStore.user.id}?${queryString}`, config).put({
 		...content,
 	}).json();
 }
 
 export function updateMyPassword(currentPassword, newPassword, confirmationPassword, config = {}) {
-	const query = qs.stringify({
-			...config,
-		},
-		{
-			encodeValuesOnly: true, // prettify URL
-		});
-
-	return useFetchValidated(`api/auth/change-password?${query}`).post({
-		currentPassword: currentPassword,
-		password: newPassword,
-		passwordConfirmation: confirmationPassword,
+	return useFetchValidated('api/auth/change-password?', config).post({
+		currentPassword: currentPassword, password: newPassword, passwordConfirmation: confirmationPassword,
 	}).json();
 }
 
-export function deleteMe(password, config = {}) {
-	const userStore = useUserStore();
-	const query = qs.stringify({
-			...config,
-		},
-		{
-			encodeValuesOnly: true, // prettify URL
-		});
+// export function deleteMe(password, config = {}) {
+// 	const userStore = useUserStore();
+// 	const query = qs.stringify({
+// 		...config,
+// 	}, {
+// 		encodeValuesOnly: true, // prettify URL
+// 	});
+//
+// 	return useFetch(
+// 		`http://localhost:1337/api/users/${userStore.user.id}?${query}`,
+// 		{
+// 			method: 'DELETE',
+// 			data: {
+// 				validationPassword: password,
+// 			},
+// 		},
+// 		true,
+// 	);
+// }
 
-	// return useFetch(
-	// 	`http://localhost:1337/api/users/${userStore.user.id}?${query}`,
-	// 	{
-	// 		method: 'DELETE',
-	// 		data: {
-	// 			validationPassword: password,
-	// 		},
-	// 	},
-	// 	true,
-	// );
-}
-
-export function updateMyPackage(id, config = {}) {
+export function updateMyPackage(id, query = {}, config = {}) {
 	const userStore = useUserStore();
-	const query = qs.stringify({
-		...config,
-	}, {
+	const queryString = qs.stringify(query, {
 		encodeValuesOnly: true,
 	});
 
-	return useFetchValidated(`api/users/${userStore.user.id}?${query}`).put({
-		validationPassword: 'TODO',
-		currentPackage: {
+	return useFetchValidated(`api/users/${userStore.user.id}?${queryString}`, config).put({
+		validationPassword: 'TODO', currentPackage: {
 			set: [id],
 		},
 	}).json();
