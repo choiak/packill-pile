ea33da68a02765996468bd4fc4cdb1f6e3ab3b55
Anson Ng <914anson5@gmail.com>
2023-05-14 21:50:59 +0800

    feat: allow to pass reactive topicId into getMyNextProblem() in utils/problem.js

--- src/utils/problem.js
+++ src/utils/problem.js
@@ -1,22 +1,25 @@
 import { getProblems } from '@/api/problem.js';
 import { getMe } from '@/api/me.js';
-import { computed, ref, watch } from 'vue';
+import { computed, unref, watch } from 'vue';
 
 export function getMyNextProblem(topicId) {
-	const id = ref();
-
-	const problemsResponse = getProblems({
-		fields: ['id'], filters: {
-			topic: topicId,
-		},
+	const problemsQuery = computed(() => {
+		return {
+			fields: ['id'], filters: {
+				topic: unref(topicId),
+			},
+		}
 	});
+
+	const problemsResponse = getProblems(problemsQuery, { immediate: false });
+
 	const problemsCompletedResponse = getMe({
-		fields: ['completedProblems'], populate: {
+		fields: [], populate: {
 			completedProblems: {
 				fields: ['id'],
 			},
 		},
-	});
+	}, { immediate: false });
 
 	const canAbort = computed(() => {
 		return problemsResponse.canAbort.value && problemsCompletedResponse.canAbort.value;
@@ -33,22 +36,51 @@ export function getMyNextProblem(topicId) {
 		}
 	}
 
+	if (unref(topicId)) {
+		abort();
+		problemsResponse.execute();
+		problemsCompletedResponse.execute();
+	}
+
+	watch(problemsQuery, () => {
+		if (unref(topicId)) {
+			abort();
+			problemsResponse.execute();
+			problemsCompletedResponse.execute();
+		}
+	});
+
 	const isLoading = computed(() => {
 		return (problemsResponse.isFetching.value || problemsCompletedResponse.isFetching.value);
 	});
 
-	watch(isLoading, (newState) => {
-		if (!newState) {
-			const problems = problemsResponse.data.value.data;
-			const problemsCompleted = problemsCompletedResponse.data.value.completedProblems;
-			const problemsUncompleted = problems.filter((problem) => !problemsCompleted.some((problemCompleted) => problemCompleted.id === problem.id));
-			if (problemsUncompleted[0]) {
-				id.value = problemsUncompleted[0].id;
-			} else {
-				id.value = 0;
-			}
+	const problems = computed(() => {
+		return problemsResponse.data.value?.data;
+	});
+
+	const problemsCompleted = computed(() => {
+		return problemsCompletedResponse.data.value?.completedProblems;
+	});
+
+	const problemsUncompleted= computed(() => {
+		if (problems.value && problemsCompleted.value) {
+			return problems.value.filter((problem) => !problemsCompleted.value.some((problemCompleted) => problemCompleted.id === problem.id));
+		} else {
+			return null;
 		}
 	});
 
-	return { id, canAbort, abort };
+	const nextProblemId = computed(() => {
+		if (problemsUncompleted.value) {
+			if (problemsUncompleted.value.length) {
+				return problemsUncompleted.value[0].id;
+			} else {
+				return 0;
+			}
+		} else {
+			return null;
+		}
+	});
+
+	return { id: nextProblemId, isLoading, canAbort, abort };
 }
