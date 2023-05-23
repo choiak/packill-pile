a32ea1e6b5aa6e7ead631b878379503f77dce92f
Anson Ng <914anson5@gmail.com>
2023-05-23 21:44:55 +0800

    feat: add route for 401.vue, 403.vue, blog.vue and announcement.vue

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,9 @@ import Packages from '@/views/workspace/packages.vue';
 import Package from '@/views/workspace/package.vue';
 import UserSettings from '@/views/settings/settings.vue';
 import General from '@/layouts/settings/general.vue';
-import NotFound from '@/views/pile/404.vue';
+import NotFound from '@/views/error/404.vue';
+import Forbidden from '@/views/error/403.vue';
+import Unauthorized from '@/views/error/401.vue';
 import Security from '@/layouts/settings/security.vue';
 import Activities from '@/layouts/user/activities.vue';
 import Projects from '@/layouts/user/projects.vue';
@@ -20,146 +22,75 @@ import DeleteAccount from '@/layouts/settings/deleteAccount.vue';
 import ProblemLoading from '@/layouts/problem/problemLoading.vue';
 import ProblemDone from '@/layouts/problem/problemDone.vue';
 import Problem from '@/layouts/problem/problem.vue';
+import Blog from '@/views/blog/blog.vue';
+import Announcement from '@/views/blog/announcement.vue';
 
-const routes = [
-	{
-		name: 'Login',
-		path: '/login',
-		component: Login,
-	},
-	{
-		name: 'Register',
-		path: '/register',
-		component: Register,
-	},
-	{
-		name: 'ForgetPassword',
-		path: '/forget-password',
-		component: ForgetPassword,
-	},
-	{
-		name: 'ResetPassword',
-		path: '/reset-password',
-		component: ResetPassword,
-	},
-	{
-		name: 'DeleteAccount',
-		path: '/delete-account',
-		component: DeleteAccount,
-	},
-	{
-		name: 'Dashboard',
-		path: '/dashboard',
-		component: Dashboard,
-	},
-	{
-		name: 'User',
-		path: '/users/:username',
-		component: User,
-		redirect: {
-			name: 'UserActivities',
-		},
-		children: [
-			{
-				name: 'UserActivities',
-				path: 'activities',
-				component: Activities,
-			},
-			{
-				name: 'userProjects',
-				path: 'projects',
-				component: Projects,
-			},
-		],
-	},
-	{
-		name: 'UserSettings',
-		path: '/settings',
-		component: UserSettings,
-		redirect: {
-			name: 'SettingsGeneral',
-		},
-		children: [
-			{
-				name: 'SettingsGeneral',
-				path: 'general',
-				component: General,
-			},
-			{
-				name: 'SettingsSecurity',
-				path: 'security',
-				component: Security,
-			},
-			{
-				name: 'SettingsSubscription',
-				path: 'subscription',
-				component: Subscription,
-			}
-		],
-	},
-	{
-		name: 'Workspace',
-		path: '/workspace/:topicId',
-		component: Workspace,
-		children: [
-			{
-				name: 'WorkspaceProblemLoading',
-				path: '',
-				component: ProblemLoading
-			},
-			{
-				name: 'WorkspaceProblemDone',
-				path: '0',
-				component: ProblemDone,
-			},
-			{
-				name: 'WorkspaceProblem',
-				path: ':problemId',
-				component: Problem,
-			}
-		]
-	},
-	{
-		name: 'Topic',
-		path: '/topics/:id',
-	},
-	{
-		name: 'Problem',
-		path: '/problems/:id',
-	},
-	{
-		name: 'Packages',
-		path: '/packages',
-		component: Packages,
-	},
-	{
-		name: 'Package',
-		path: '/packages/:id',
-		component: Package,
-	},
-	{
-		name: '404',
-		path: '/404',
-		component: NotFound,
-	},
-	{
-		path: '/:pathMatch(.*)*',
-		redirect: '/404',
-	},
-];
+const routes = [{
+	name: 'Login', path: '/login', component: Login,
+}, {
+	name: 'Register', path: '/register', component: Register,
+}, {
+	name: 'ForgetPassword', path: '/forget-password', component: ForgetPassword,
+}, {
+	name: 'ResetPassword', path: '/reset-password', component: ResetPassword,
+}, {
+	name: 'DeleteAccount', path: '/delete-account', component: DeleteAccount,
+}, {
+	name: 'Dashboard', path: '/dashboard', component: Dashboard,
+}, {
+	name: 'User', path: '/users/:username', component: User, redirect: {
+		name: 'UserActivities',
+	}, children: [{
+		name: 'UserActivities', path: 'activities', component: Activities,
+	}, {
+		name: 'userProjects', path: 'projects', component: Projects,
+	}],
+}, {
+	name: 'UserSettings', path: '/settings', component: UserSettings, redirect: {
+		name: 'SettingsGeneral',
+	}, children: [{
+		name: 'SettingsGeneral', path: 'general', component: General,
+	}, {
+		name: 'SettingsSecurity', path: 'security', component: Security,
+	}, {
+		name: 'SettingsSubscription', path: 'subscription', component: Subscription,
+	}],
+}, {
+	name: 'Workspace', path: '/workspace/:topicId', component: Workspace, children: [{
+		name: 'WorkspaceProblemLoading', path: '', component: ProblemLoading,
+	}, {
+		name: 'WorkspaceProblemDone', path: '0', component: ProblemDone,
+	}, {
+		name: 'WorkspaceProblem', path: ':problemId', component: Problem,
+	}],
+}, {
+	name: 'Topic', path: '/topics/:id',
+}, {
+	name: 'Problem', path: '/problems/:id',
+}, {
+	name: 'Packages', path: '/packages', component: Packages,
+}, {
+	name: 'Package', path: '/packages/:id', component: Package,
+}, {
+	name: 'Blog', path: '/blog', component: Blog,
+}, {
+	name: 'Announcement', path: '/blog/:announcementId', component: Announcement,
+}, {
+	name: '401', path: '/401', component: Unauthorized,
+}, {
+	name: '403', path: '/403', component: Forbidden,
+}, {
+	name: '404', path: '/404', component: NotFound,
+}, {
+	path: '/:pathMatch(.*)*', redirect: '/404',
+}];
 
 const router = createRouter({
-	history: createWebHistory(),
-	routes,
+	history: createWebHistory(), routes,
 });
 
 router.beforeEach(async (to, from) => {
-	if (
-		to.name !== 'Login' &&
-		to.name !== 'Register' &&
-		to.name !== 'ForgetPassword' &&
-		!(await validateToken())
-	) {
+	if (to.name !== 'Login' && to.name !== 'Register' && to.name !== 'ForgetPassword' && !(await validateToken())) {
 		return { name: 'Login' };
 	}
 });
