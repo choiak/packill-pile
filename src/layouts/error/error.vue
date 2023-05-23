3c91415097c94c5cddde2fe067f3aba43a44b05c
Anson Ng <914anson5@gmail.com>
2023-05-23 21:42:26 +0800

    refactor: a generic error.vue layout is created, 404.vue now uses the error.vue layout

---
+++ src/layouts/error/error.vue
@@ -0,0 +1,38 @@
+<template>
+	<div
+		class="flex h-screen w-screen flex-col justify-between bg-orange-600 p-8"
+	>
+		<div>
+			<button
+				class="flex w-fit items-center space-x-1 rounded-lg p-2 text-white transition hover:bg-white hover:text-black hover:shadow active:shadow-blue-900"
+				@click.prevent="router.back()"
+			>
+				<ArrowLeftCircleIcon class="h-5 w-5" />
+				<label>Back</label>
+			</button>
+		</div>
+		<div class="flex items-end justify-between">
+			<div class="space-y-4 max-w-[512px]">
+				<div class='flex space-x-4 items-baseline'>
+					<h1 class="font-semibold drop-shadow-xl text-white">{{ code }}</h1>
+					<h5 class="font-medium uppercase drop-shadow-xl text-white">{{ statusText }}</h5>
+				</div>
+				<h6 class="leading-5 drop-shadow-xl text-white">{{ description }}</h6>
+			</div>
+			<div class="max-w-[25%] space-y-4">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import router from '@/router/index.js';
+import { ArrowLeftCircleIcon } from '@heroicons/vue/24/outline/index.js';
+
+const props = defineProps({
+	code: Number,
+	statusText: String,
+	description: String,
+});
+</script>
---
+++ src/views/error/404.vue
@@ -0,0 +1,30 @@
+<template>
+	<Error :code='404'
+		   status-text='Not Found'
+		   description="Oops, something's missing or we have moved the page to
+		   somewhere else.">
+		<h5
+			class='w-fit rounded-lg bg-blue-600 px-2 font-semibold uppercase tracking-widest text-white'
+		>
+			What is 404?
+		</h5>
+		<div class='prose font-text text-white drop-shadow-xl'>
+			<p>
+				The 404 status code belongs to the HTTP (Hypertext
+				Transfer Protocol) standard response codes. This error
+				pops up when the client has successfully requested from
+				or communicated with the server, but the server is
+				unable to find the information that was requested.
+				Common causes are:
+			</p>
+			<ul aria-label="Causes of 404 error">
+				<li>Mistyped URL; or</li>
+				<li>the page has been moved or deleted.</li>
+			</ul>
+		</div>
+	</Error>
+</template>
+
+<script setup>
+import Error from '@/layouts/error/error.vue';
+</script>
