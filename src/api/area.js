50006d4aa411f8249c4ea0cbb5203dcbb429e77e
Anson Ng <914anson5@gmail.com>
2023-06-23 20:25:01 +0800

    feat: allow reactive arguments to be passed to area.js

--- src/api/area.js
+++ src/api/area.js
@@ -1,12 +1,19 @@
 import { useFetchValidated } from '@/utils/fetch.js';
 import qs from 'qs';
+import { computed, unref } from 'vue';
 
-export function getAreas(config = {}) {
-	const query = qs.stringify({ ...config },
-		{
-			encodeValuesOnly: true, // prettify URL
-		},
-	);
+export function getAreas(query = {}, config = {}) {
+	const queryString = computed(() => {
+		return qs.stringify(unref(query),
+			{
+				encodeValuesOnly: true,
+			},
+		);
+	});
 
-	return useFetchValidated(`api/areas?${query}`).get().json();
+	const url = computed(() => {
+		return `api/areas?${queryString.value}`;
+	});
+
+	return useFetchValidated(url, config).get().json();
 }
