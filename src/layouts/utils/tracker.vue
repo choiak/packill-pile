777ad5e94d54429fb434139a95975168c3332d38
Anson Ng <914anson5@gmail.com>
2023-06-26 16:08:03 +0800

    feat: tracker.vue now works correctly when package is empty or finished

--- src/layouts/utils/tracker.vue
+++ src/layouts/utils/tracker.vue
@@ -1,10 +1,13 @@
 <template>
-	<div>
-		<div>
+	<div v-if='currentPackage'>
 			<p class='text-xs text-neutral-500 font-medium'>{{ currentPackageName }}</p>
-			<router-link to='/topics' class='flex items-center space-x-1 hover:underline hover:underline-offset-2 hover:decoration-sky-800 '>
-				<p class='font-semibold text-blue-600 font-display'>{{ currentPartitionName }}</p>
-				<ArrowUpRightIcon class='text-blue-600 h-3 w-3 stroke-2'/>
+			<router-link to='/topics'
+						 class='flex items-center space-x-1 hover:underline hover:underline-offset-2 hover:decoration-sky-800 '>
+				<div>
+					<p class='font-semibold text-blue-600 font-display' v-if='isCurrentPackageCompleted'>Overview</p>
+					<p class='font-semibold text-blue-600 font-display' v-else>{{ currentPartitionName }}</p>
+				</div>
+				<ArrowUpRightIcon class='text-blue-600 h-3 w-3 stroke-2' />
 			</router-link>
 			<VenustTooltip>
 				<template #reference>
@@ -14,10 +17,18 @@
 					</div>
 				</template>
 				<template #tooltip>
-					You have completed {{currentPartitionProgressPercentage}}% of the partition so far.
+					<span v-if='isCurrentPackageCompleted'>Hooray! You have completed {{ currentPackageName }}.</span>
+					<span v-else>You have completed {{ currentPartitionProgressPercentage }}% of the partition so far.</span>
 				</template>
 			</VenustTooltip>
-		</div>
+	</div>
+	<div v-else>
+		<p class='text-xs text-neutral-500 font-medium'>Select your package to get started</p>
+		<router-link to='/packages'
+					 class='flex items-center space-x-1 hover:underline hover:underline-offset-2 hover:decoration-sky-800 '>
+			<p class='font-semibold text-blue-600 font-display'>Unpack a new package</p>
+			<ArrowUpRightIcon class='text-blue-600 h-3 w-3 stroke-2' />
+		</router-link>
 	</div>
 </template>
 
@@ -28,9 +39,9 @@ import { storeToRefs } from 'pinia';
 import { ArrowUpRightIcon } from '@heroicons/vue/24/outline/index.js';
 import VenustTooltip from '@/components/venust/tooltip/venustTooltip.vue';
 
-const myStore = storeToRefs(useMyStore());
+const myStore = useMyStore();
 
-const { currentPackage, currentPartition, currentPartitionProgress, } = myStore;
+const { currentPackage, currentPartition, currentPartitionProgress, isCurrentPackageCompleted } = storeToRefs(myStore);
 
 const currentPackageName = computed(() => {
 	return currentPackage.value?.name;
@@ -43,6 +54,8 @@ const currentPartitionName = computed(() => {
 const currentPartitionProgressPercentage = computed(() => {
 	if (currentPartitionProgress.value) {
 		return Math.round(currentPartitionProgress.value * 100);
+	} else if (isCurrentPackageCompleted.value) {
+		return 100;
 	} else {
 		return 0;
 	}
